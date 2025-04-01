057b7912951c1f916fd5a658c216d475d3c6a0fb
Adheesh Garg <adheeshgarg0611@gmail.com>
2025-04-02 01:34:25 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import { fibonacci } from "./fibonacci.js";
+import { gcd } from "./gcd.js";
+
+const num = 5;
+console.log(`Fibonacci(${num}):`, fibonacci(num));
+console.log(`GCD(${num}, 10):`, gcd(num, 10));
